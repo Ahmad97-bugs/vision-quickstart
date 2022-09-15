7e3e042ad059b592715bd7d21c78b1137102265b
Ahmad97-bugs <73170682+Ahmad97-bugs@users.noreply.github.com>
2022-09-15 14:46:58 +0300

    Initial commit

---
+++ app/src/main/java/com/google/mlkit/vision/demo/java/posedetector/classification/PoseClassifierProcessor.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright 2020 Google LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.mlkit.vision.demo.java.posedetector.classification;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.annotation.WorkerThread;
+
+import com.google.common.base.Preconditions;
+import com.google.mlkit.vision.pose.Pose;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+
+/**
+ * Accepts a stream of {@link Pose} for classification and Rep counting.
+ */
+public class PoseClassifierProcessor {
+    private static final String TAG = "PoseClassifierProcessor";
+    private static final String POSE_SAMPLES_FILE = "pose/fitness_poses_csvs_out.csv";
+    public static boolean isCount = false;
+
+
+    // Specify classes for which we want rep counting.
+    // These are the labels in the given {@code POSE_SAMPLES_FILE}. You can set your own class labels
+    // for your pose samples.
+    private static final String JUMP_CLASS = "squatjump_jump";
+    private static final String DOWN_CLASS = "squatjump_down";
+    private static final String STAND_CLASS = "squatjump_stand";
+    private static final String[] POSE_CLASSES = {
+            JUMP_CLASS, DOWN_CLASS, STAND_CLASS
+    };
+
+    private final boolean isStreamMode;
+
+    private EMASmoothing emaSmoothing;
+    private static List<RepetitionCounter> repCounters;
+    private PoseClassifier poseClassifier;
+    private String lastRepResult;
+
+    @WorkerThread
+    public PoseClassifierProcessor(Context context, boolean isStreamMode) {
+        Preconditions.checkState(Looper.myLooper() != Looper.getMainLooper());
+        this.isStreamMode = isStreamMode;
+        if (isStreamMode) {
+            emaSmoothing = new EMASmoothing();
+            repCounters = new ArrayList<>();
+            lastRepResult = "";
+        }
+        loadPoseSamples(context);
+    }
+
+    private void loadPoseSamples(Context context) {
+        // load provided pose samples
+        List<PoseSample> poseSamples = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(context.getAssets().open(POSE_SAMPLES_FILE)));
+            String csvLine = reader.readLine();
+            while (csvLine != null) {
+                // If line is not a valid {@link PoseSample}, we'll get null and skip adding to the list.
+                PoseSample poseSample = PoseSample.getPoseSample(csvLine, ",");
+                if (poseSample != null) {
+                    poseSamples.add(poseSample);
+                }
+                csvLine = reader.readLine();
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Error when loading pose samples.\n" + e);
+        }
+        // create counter for each pose initiated
+        poseClassifier = new PoseClassifier(poseSamples);
+        if (isStreamMode) {
+            for (String className : POSE_CLASSES) {
+                repCounters.add(new RepetitionCounter(className));
+            }
+        }
+    }
+
+    /**
+     * Given a new {@link Pose} input, returns a list of formatted {@link String}s with Pose
+     * classification results.
+     *
+     * <p>Currently it returns up to 2 strings as following:
+     * 0: PoseClass : X reps
+     * 1: PoseClass : [0.0-1.0] confidence
+     */
+    @WorkerThread
+    public List<String> getPoseResult(Pose pose) {
+        Preconditions.checkState(Looper.myLooper() != Looper.getMainLooper());
+        List<String> result = new ArrayList<>();
+        ClassificationResult classification = poseClassifier.classify(pose);
+
+        // Update {@link RepetitionCounter}s if {@code isStreamMode}.
+        if (isStreamMode && isCount) {
+            // Feed pose to smoothing even if no pose found.
+            classification = emaSmoothing.getSmoothedResult(classification);
+
+            // Return early without updating repCounter if no pose found.
+            if (pose.getAllPoseLandmarks().isEmpty()) {
+                result.add(lastRepResult);
+                return result;
+            }
+
+            for (RepetitionCounter repCounter : repCounters) {
+                int repsBefore = repCounter.getNumRepeats();
+                int repsAfter = repCounter.addClassificationResult(classification, pose);
+                if (repsAfter > repsBefore) {
+                    // Play a fun beep when rep counter updates.
+//          ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, 100);
+//          tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+                    lastRepResult = String.format(
+                            Locale.US, "%s : %d reps", repCounter.getClassName(), repsAfter);
+                    break;
+                }
+            }
+            result.add(lastRepResult);
+        }
+
+        // Add maxConfidence class of current frame to result if pose is found.
+        if (!pose.getAllPoseLandmarks().isEmpty()) {
+            String maxConfidenceClass = classification.getMaxConfidenceClass();
+            String maxConfidenceClassResult = String.format(
+                    Locale.US,
+                    "%s : %.2f confidence",
+                    maxConfidenceClass,
+                    classification.getClassConfidence(maxConfidenceClass)
+                            / poseClassifier.confidenceRange());
+
+            result.add(maxConfidenceClassResult);
+        }
+        return result;
+    }
+
+    public static double calculatedJumpHeight() {
+        RepetitionCounter jumpRecord = repCounters.get(0);
+        List<Pose> poseList = jumpRecord.getPoseList();
+        List<Long> timeList = jumpRecord.getTimeList();
+        if (!poseList.isEmpty()) {
+            float firstHeelIndex = (poseList.get(0).getPoseLandmark(31).getPosition().y + poseList.get(0).getPoseLandmark(32).getPosition().y) / 2;
+            float highestPoint = firstHeelIndex;
+            float point;
+            int count = 0;
+            int highestCount = 0;
+
+            for (Pose p : poseList) {
+                point = (p.getPoseLandmark(31).getPosition().y + p.getPoseLandmark(32).getPosition().y) / 2;
+                if (point > highestPoint) {
+                    highestPoint = point;
+                    highestCount = count;
+                }
+                count++;
+            }
+            Long start = timeList.get(0);
+            Long stop = timeList.get(highestCount);
+            int timeInAir = (int) (stop - start) / 1000;
+
+            double jumpHeight = timeInAir * timeInAir * 1.22625;
+            return jumpHeight;
+
+        }
+        return (double) 0;
+    }
+
+}
---
+++ app/src/main/java/com/google/mlkit/vision/demo/java/posedetector/classification/RepetitionCounter.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2020 Google LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.mlkit.vision.demo.java.posedetector.classification;
+
+import com.google.mlkit.vision.pose.Pose;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Counts reps for the give class.
+ */
+public class RepetitionCounter {
+    // These thresholds can be tuned in conjunction with the Top K values in {@link PoseClassifier}.
+    // The default Top K value is 10 so the range here is [0-10].
+    private static final float DEFAULT_ENTER_THRESHOLD = 6f;
+    private static final float DEFAULT_EXIT_THRESHOLD = 4f;
+
+    private final String className;
+    private final float enterThreshold;
+    private final float exitThreshold;
+
+    private int numRepeats;
+    private boolean poseEntered;
+    private List<Long> timeList;
+    private List<Pose> poseList;
+
+    public RepetitionCounter(String className) {
+        this(className, DEFAULT_ENTER_THRESHOLD, DEFAULT_EXIT_THRESHOLD);
+    }
+
+    public RepetitionCounter(String className, float enterThreshold, float exitThreshold) {
+        this.className = className;
+        this.enterThreshold = enterThreshold;
+        this.exitThreshold = exitThreshold;
+        numRepeats = 0;
+        poseEntered = false;
+        timeList = new ArrayList<Long>();
+        poseList = new ArrayList<Pose>();
+    }
+
+    /**
+     * Adds a new Pose classification result and updates reps for given class.
+     *
+     * @param classificationResult {link ClassificationResult} of class to confidence values.
+     * @return number of reps.
+     */
+    public int addClassificationResult(ClassificationResult classificationResult, Pose pose) {
+        float poseConfidence = classificationResult.getClassConfidence(className);
+
+        if (!poseEntered) {
+            poseEntered = poseConfidence > enterThreshold;
+            return numRepeats;
+        }
+
+        if (poseConfidence < exitThreshold) {
+            numRepeats++;
+            poseEntered = false;
+            timeList.add(new Date().getTime());
+            poseList.add(pose);
+        }
+
+        return numRepeats;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public int getNumRepeats() {
+        return numRepeats;
+    }
+
+    public List<Pose> getPoseList(){
+        return poseList;
+    }
+
+    public List<Long> getTimeList(){
+        return timeList;
+    }
+}
