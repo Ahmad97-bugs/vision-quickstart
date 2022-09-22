851340beb0a117892a9076a9e5398286c615e663
Ahmad97-bugs <73170682+Ahmad97-bugs@users.noreply.github.com>
2022-09-22 17:31:37 +0300

    register activity

--- app/src/main/java/com/google/mlkit/vision/demo/java/GUI/MainActivity.java
+++ app/src/main/java/com/google/mlkit/vision/demo/java/GUI/MainActivity.java
@@ -1,11 +1,22 @@
 package com.google.mlkit.vision.demo.java.GUI;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.mlkit.vision.demo.R;
+import com.google.mlkit.vision.demo.java.ChooserActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -16,5 +27,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        Button loginBtn = this.<Button>findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+                String email = editTextEmail.getText().toString();
+                EditText editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+                String password = editTextPassword.getText().toString();
+                if ((email.equals("admin")) && (password.equals("admin"))) {
+                    Toast.makeText(getApplicationContext(), "Hello Admin", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this, ChooserActivity.class);
+                    startActivity(intent);
+                } else {
+                    // validate user using firebase
+                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(user);
+                                Toast.makeText(MainActivity.this, "Hello, "+user.getEmail(),
+                                        Toast.LENGTH_SHORT).show();
+                                flag=true;
+                            } else {
+                                Toast.makeText(MainActivity.this, "Sorry, your password was incorrect.",
+                                        Toast.LENGTH_SHORT).show();
+
+                            }
+
+                        }
+                    });
+
+                    if(flag)
+                        Toast.makeText(getApplicationContext(), "Please check the email and password again", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
+
+        registerBtn = findViewById(R.id.registerBtn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UserRegister.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void updateUI(FirebaseUser user){
+        if(user != null){
+//            DataBaseManager.getInstance().openDataBase(this);
+//            Intent intent = new Intent(this, MainPage.class);
+//            startActivity(intent);
+            finish();
+        }
     }
 }
---
+++ app/src/main/java/com/google/mlkit/vision/demo/java/GUI/UserRegister.java
@@ -0,0 +1,14 @@
+package com.google.mlkit.vision.demo.java.GUI;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.mlkit.vision.demo.R;
+
+public class UserRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_register);
+    }
+}
