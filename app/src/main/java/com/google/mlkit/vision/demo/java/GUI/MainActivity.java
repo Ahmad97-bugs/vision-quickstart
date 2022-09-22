91d3c9b3e77b77afdf727f968beec071e9e34aba
Ahmad97-bugs <73170682+Ahmad97-bugs@users.noreply.github.com>
2022-09-22 14:13:09 +0300

    git ignore, starting GUI

---
+++ app/src/main/java/com/google/mlkit/vision/demo/java/GUI/MainActivity.java
@@ -0,0 +1,15 @@
+package com.google.mlkit.vision.demo.java.GUI;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.mlkit.vision.demo.R;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
