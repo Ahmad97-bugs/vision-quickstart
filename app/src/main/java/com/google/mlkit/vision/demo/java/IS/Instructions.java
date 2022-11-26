37a52116e93fc5538e51e2d73f4f54db6976f997
Ahmad97-bugs <73170682+Ahmad97-bugs@users.noreply.github.com>
2022-11-26 19:19:03 +0200

    updating instructions

--- app/src/main/java/com/google/mlkit/vision/demo/java/IS/Instructions.java
+++ app/src/main/java/com/google/mlkit/vision/demo/java/IS/Instructions.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * intent to show the user examples on how to jump
@@ -18,12 +19,16 @@ public class Instructions extends AppCompatActivity{
     ImageView jump;
     Button next;
     Button back;
+    TextView ins;
     int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
+
+        ins = findViewById(R.id.instuctions);
+        ins.setText("Stand with your arms next to your body");
 
         stand = findViewById(R.id.stand);
         down = findViewById(R.id.down);
@@ -55,18 +60,21 @@ public class Instructions extends AppCompatActivity{
 
     private void updatePicture(){
         if(count == 0){
+            ins.setText("Stand with your arms next to your body");
             next.setVisibility(View.VISIBLE);
             back.setVisibility(View.INVISIBLE);
             down.setVisibility(View.INVISIBLE);
             jump.setVisibility(View.INVISIBLE);
             stand.setVisibility(View.VISIBLE);
         } else if(count == 1){
+            ins.setText("Getting into squat position with your hands moving with your hips");
             next.setVisibility(View.VISIBLE);
             back.setVisibility(View.VISIBLE);
             down.setVisibility(View.VISIBLE);
             jump.setVisibility(View.INVISIBLE);
             stand.setVisibility(View.INVISIBLE);
         } else if(count == 2){
+            ins.setText("Jump as you lift your arms into the air");
             next.setVisibility(View.INVISIBLE);
             back.setVisibility(View.VISIBLE);
             down.setVisibility(View.INVISIBLE);
